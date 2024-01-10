bb12bc1c20ddf0b804b48eb93e050754ce91f693
Ojee <omargamal.02.11@gmail.com>
2024-01-10 17:29:47 +0100

    fix

--- serverPharmacy/src/routes/controller.js
+++ serverPharmacy/src/routes/controller.js
@@ -536,6 +536,7 @@ const createOrder = async (req, res) => {
                 res.status(400).json("Insufficient quantity");
             } else {
                 medicine.quantity -= item.quantity;
+                await medicine.save();
                 amount += item.quantity * medicine.price;
             }
         }
